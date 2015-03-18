d90fad16ef771830d21f7d425f3c385695dfc719
p10ns11y <sathyam.peram@gmail.com>
2015-03-18 15:31:53 +0100

    Trepn profiler link added

--- ui.R
+++ ui.R
@@ -5,7 +5,9 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput('file1', 'Load Trepn Profiler log file .db',
-                accept=c('.db'))
+                accept=c('.db')),
+      p("Treph profiler can be dowloaded from here"),
+      a(href='https://developer.qualcomm.com/mobile-development/increase-app-performance/trepn-plug-eclipse/trepn-plug-eclipse-getting-started','Trepn for Eclipse')
       
     ),
     mainPanel(
