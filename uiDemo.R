2b9d46cb1b713a9310707679607f1642f0146e03
p10ns11y <sathyam.peram@gmail.com>
2015-03-18 12:50:19 +0100

    Fresh start to support file upload for data processing

---
+++ uiDemo.R
@@ -0,0 +1,26 @@
+# Author : Peramanathan Sathyamoorthy
+# User Display
+
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel("BatBoomer Energy Analyser"),
+  sidebarPanel(
+    radioButtons('level','Choose the level',
+                 c("Platform Active"="1",
+                   "Platform Idle"="2",
+                   "Application Active"="3",
+                   "Application Idle"="4")),
+    
+    fileInput('file1', 'Choose .db File',
+              accept=c('.db'))
+  ),
+  
+  mainPanel(
+    h3('Analysed Results for'),
+    verbatimTextOutput("olevel"),
+    verbatimTextOutput("oDeviceInfo"),
+    verbatimTextOutput("platformTable")
+  )
+  
+))
